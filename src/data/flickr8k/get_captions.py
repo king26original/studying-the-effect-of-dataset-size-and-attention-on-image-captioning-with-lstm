ecadcf482a23d5c7bbd5d7c57126d84f5dbbbfbc
king26original <analogsns@gmail.coM>
2026-06-23 12:33:56 +0530

    Update get_captions.py

--- src/data/flickr8k/get_captions.py
+++ src/data/flickr8k/get_captions.py
@@ -1,31 +1,27 @@
 import random
 from src.data.flickr8k.download_flickr8k import download
 
-download()
+def get_captions(train_file, test_file, caption_file):
+  with open(train_file) as f:
+    images=[line.strip() for line in f]
 
-train_file="flickr8k/Flickr_8k.trainImages.txt"
+  with open(test_file) as f:
+    test_images=[line.strip() for line in f]
 
-with open(train_file) as f:
-  images=[line.strip() for line in f]
+  images=set(images)
+  test_images=set(test_images)
 
-test_file="flickr8k/Flickr_8k.testImages.txt"
+  captions={}
+  test_captions={}
 
-with open(test_file) as f:
-  test_images=[line.strip() for line in f]
+  with open(caption_file) as f:
+    for line in f:
+      image_id,caption=line.strip().split('\t')
+      image_id=image_id.split('#')[0]
 
-images=set(images)
-test_images=set(test_images)
-
-captions={}
-test_captions={}
-
-caption_file="/kaggle/working/flickr8k/Flickr8k.token.txt"
-with open(caption_file) as f:
-  for line in f:
-    image_id,caption=line.strip().split('\t')
-    image_id=image_id.split('#')[0]
-
-    if image_id in images:
-      captions.setdefault(image_id, []).append(caption.lower())
-    elif image_id in test_images:
-      test_captions.setdefault(image_id, []).append(caption.lower())
+      if image_id in images:
+        captions.setdefault(image_id, []).append(caption.lower())
+      elif image_id in test_images:
+        test_captions.setdefault(image_id, []).append(caption.lower())
+  
+  return captions, test_captions
